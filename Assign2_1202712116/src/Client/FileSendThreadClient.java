1d56bfc4d1a85806363f10a7dec6a752fc89d234
James Harris <wizardofmath@gmail.com>
2012-11-06 14:36:01 -0700

    Made small changes to stop the add error.

--- Assign2_1202712116/src/Client/FileSendThreadClient.java
+++ Assign2_1202712116/src/Client/FileSendThreadClient.java
@@ -24,11 +24,17 @@ public class FileSendThreadClient extends Thread {
 	private String fileName;
 	private MusicApp parent;
 
-	public FileSendThreadClient(String file, MusicApp parent, Socket socket) throws UnknownHostException, IOException {
-		this.fileName = file;
-		this.setParent(parent);
-		this.socket = socket;
-		out = new DataOutputStream(socket.getOutputStream());
+	public FileSendThreadClient(String file, MusicApp parent, Socket socket) {
+
+		try {
+			this.fileName = file;
+			this.setParent(parent);
+			this.socket = socket;
+			out = new DataOutputStream(socket.getOutputStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public Socket getSocket() {
@@ -67,7 +73,6 @@ public class FileSendThreadClient extends Thread {
 		try {
 			streamFile();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -76,18 +81,18 @@ public class FileSendThreadClient extends Thread {
 		File file = new File(fileName);
 		System.out.print("Keep Going....");
 		FileInputStream fileStream = new FileInputStream(file);
-		byte[] buffer = new byte[1024];
-		int size = fileStream.read(buffer);
+		byte[] buffer = new byte[4*1024];
+		int size = fileStream.read(buffer);;
 		while (size > 0) {
+			System.out.println("Before =");
 			out.write(buffer, 0, size);
 			size = fileStream.read(buffer);
+			System.out.print(" =After ");
 		}
-		out.flush();
-		out.close();
 		fileStream.close();
-		System.out.print("Done....");
 		this.socket.close();
-		new Popup("Upload Complete of " +fileName).start();
+		System.out.print("Done....");
+
 	}
 
 }
